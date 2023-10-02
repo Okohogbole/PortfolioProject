8947970b713b669a293981d3661a30541e740e36
Okohogbole <110495804+Okohogbole@users.noreply.github.com>
2023-10-02 17:57:36 +0100

    Add files via upload
    File Containing bikeshare data extracted from SQL database server , explored using SQL Server Management Studio19.

---
+++ SQLQuery_Bike_Stores1.sql
@@ -0,0 +1,67 @@
+---View the tables contining the datasets
+SELECT *
+FROM [production].[categories]
+
+SELECT *
+FROM [production].[products]
+
+SELECT *
+FROM [production].[brands]
+
+SELECT *
+FROM [production].[stocks]
+
+
+SELECT *
+FROM [sales].[customers]
+
+SELECT *
+FROM [sales].[orders]
+
+SELECT *
+FROM [sales].[order_items]
+
+SELECT *
+FROM [sales].[stores]
+
+SELECT *
+FROM [sales].[staffs]
+
+
+--- Extract specific data needed and perform calculations
+--- The query pulls out data from several tables using JOINs to identify orders , customer detils , product details (Name,quantity, category) and revenue as well as store and sales rep details.
+SELECT
+ord.Order_id,
+CONCAT(cus.first_name,' ',cus.last_name) Customer_Name,
+cus.City,
+cus.State,
+ord.Order_date,
+SUM(ite.quantity) AS 'Total Units',---for total sales 
+SUM(ite.quantity * ite.list_price) AS 'Revenue',---revenue generated
+pro.product_name,
+cat.category_name,
+sto.store_name,
+CONCAT(staff.first_name,' ',staff.last_name) AS 'Rep_Name'
+FROM Sales.Orders ord
+JOIN Sales.Customers cus
+ON  ord.Customer_id = cus.Customer_id
+JOIN Sales.Order_items ite
+ON ord.order_id = ite.order_id
+JOIN Production.Products pro
+ON ite.product_id = pro.product_id
+JOIN production.categories cat
+ON pro.category_id = cat.category_id
+JOIN sales.stores sto
+ON ord.store_id = sto.store_id
+JOIN sales.staffs staff
+ON ord.staff_id = staff.staff_id
+GROUP BY ---since we are using aggregate functions
+ord.Order_id,
+CONCAT(cus.first_name,' ',cus.last_name),
+cus.City,
+cus.State,
+ord.Order_date,
+pro.product_name,
+cat.category_name,
+sto.store_name,
+CONCAT(staff.first_name,' ',staff.last_name)
